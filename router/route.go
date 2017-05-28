56632a6b727ecc2f6a4f19957b8a148245f31d3e
Tamara Kaufler <tkaufler@gmail.com>
2017-05-28 15:29:47 +0100

    Next implementation draft

--- router/route.go
+++ router/route.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"context"
 	"errors"
 	"net/http"
 	"regexp"
@@ -10,16 +11,16 @@ import (
 // Route - represents configuration for a path segment
 type Route struct {
 	Segment  string
-	IsParam  bool
-	Handlers map[string]http.Handler // key=HTTP method, value is the handler
-	Routes   map[string]*Route       // represents oprional children segments. key=segment. If no hild Routes => invoke the handler
+	IsParam  map[string]bool    // key = method, indicates whether this is a named parameter
+	Handlers map[string]Handler // key = HTTP method, value is the handler
+	Routes   map[string]*Route  // represents oprional children segments. key = segment. If no hild Routes => invoke the handler
 }
 
 func getSegments(path string) ([]string, error) {
 	var ok bool
 	var err error
 
-	// sanity chec, thatthe path starts with a slash
+	// sanity check, that the path starts with a slash
 	if ok, err = regexp.MatchString("^/", path); err == nil {
 		return nil, err
 	}
@@ -39,40 +40,106 @@ func getSegments(path string) ([]string, error) {
 	return segments[1:len], nil
 }
 
-func processSegment(seg string, routes map[string]*Route, method string, handler http.Handler) (*Route, error) {
-	var err error
-	var ok bool
+func processSegment(i int, requestPath RequestPath) error {
 
-	route, ok := routes[seg]
+	var (
+		err      error
+		ok       bool
+		isParam  map[string]bool
+		handlers map[string]Handler
+		routes   map[string]*Route
+	)
+
+	segments := requestPath.Segments
+	method := requestPath.Method
+	handler := requestPath.Handler
+
+	seg := segments[i]
+	route, ok := requestPath.Routes[seg]
 
 	// a new segment => create a new child tree node/Route
 	if !ok {
-		handlers := make(map[string]http.Handler)
+		isParam = make(map[string]bool)
+		handlers = make(map[string]Handler)
 		routes = make(map[string]*Route)
 		route = &Route{
 			Segment:  seg,
-			IsParam:  false,
+			IsParam:  isParam,
 			Handlers: handlers,
 			Routes:   routes,
 		}
 	}
 
 	if ok, err = regexp.MatchString("^:", seg); err != nil {
-		return nil, err
+		return err
 	}
 	if ok {
-		route.IsParam = true
+		route.IsParam[method] = true
 	}
-
-	routes = route.Routes
 
 	sl := len(segments)
 
+	//base case of the recursive method
 	//associate the path with a handler
 	if i == (sl - 1) {
 		route.Handlers[method] = handler
+		return nil
 	}
 
-	routes[method] = route
-	route.Routes = routes
+	//recursion
+	requestPath.Routes[seg] = route
+
+	err = processSegment(i+1, requestPath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func processRequest(segments []string, req *http.Request, route *Route, i int) (Handler, error) {
+
+	var ok bool
+	var handler Handler
+
+	method := req.Method
+
+	// base case 1 of the recursive method
+	//		path ... /
+	if len(segments) == 0 {
+		if handler, ok = route.Handlers[method]; !ok {
+			return nil, errors.New("No handler for " + req.URL.RawPath)
+		}
+	}
+
+	segment := segments[i]
+
+	if _, ok = route.IsParam[method]; ok {
+		ctx := req.Context()
+		ctx = context.WithValue(ctx, route.Segment, segment)
+		req = req.WithContext(ctx)
+	}
+
+	// base case 2 of the recursive method
+	//		returning when this is the last segment
+	if i == (len(segments) - 1) {
+		if handler, ok = route.Handlers[method]; !ok {
+			return nil, errors.New("No handler for " + req.URL.RawPath)
+		}
+		return handler, nil
+	}
+
+	routes := route.Routes
+
+	if routes == nil {
+		return nil, errors.New("Error in route registration: Missing child Routes at " + route.Segment)
+	}
+
+	if route, ok = routes[segment]; !ok {
+		return nil, errors.New("Error in route registration: Missing Route at " + route.Segment)
+	}
+
+	// recursion into child routes
+	processRequest(segments, req, route, i+1)
+	return handler, nil
 }
--- router/router.go
+++ router/router.go
@@ -1,41 +1,64 @@
 package router
 
 import (
-	"context"
+	"log"
 	"net/http"
-	"regexp"
 )
+
+type Handler func(http.ResponseWriter, *http.Request)
 
 // Router - Router object needs to satisfy this interface to be a handler
 type Router interface {
-	http.Handler // ie implements the ServeHTTP method, writes the response out to the client
-	GET(path string, handler http.Handler)
-	POST(path string, handler http.Handler)
-	DELETE(path string, handler http.Handler)
-	PUT(path string, handler http.Handler)
+	ServeHTTP(http.ResponseWriter, *http.Request) // ie implements the ServeHTTP method, writes the response out to the client
+	GET(path string, handler Handler)
+	POST(path string, handler Handler)
+	DELETE(path string, handler Handler)
+	PUT(path string, handler Handler)
 }
 
 // Mux object
 type Mux struct {
-	Context     context.Context
-	RootSegment string
-	Params      map[string]interface{} //?
-	Routes      map[string]*Route
+	BaseRoute *Route
+}
+
+// RequestPath type
+type RequestPath struct {
+	Segments []string
+	Routes   map[string]*Route
+	Method   string
+	Handler  Handler
 }
 
 // ServeHTTP method uses the paths and their handlers
 // to execure the correct handler for the request URL
 func (m *Mux) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	// split the req.Url.Path
+
+	// extract the parameters, if any => add as a context parameter
+	// get the relevant handler
+	// users of this package, using named parameters
+	// have access to them through the request context and the
+	// parameter name used when registering the url, eg:
+	// 		req.Context().Value("Username");
+	segments, err := getSegments(req.URL.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	/*
-		segments, err := getSegments(req.URL.Path)
-		if err != nil {
-			panic(e)
+		method := req.Method
+		if len(segments) == 0 {
+			if handler, ok := m.BaseRoute.Handlers[method]; !ok {
+				log.Panic("No handler for " + req.URL.RawPath)
+			}
 		}
 	*/
-	// create the Routes (Route trie tree)
-	// extract the parameters, if any => attach as Params
-	// get the handler and execute it
+
+	handler, err := processRequest(segments, req, m.BaseRoute, -1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler(res, req)
 }
 
 // New method for creating a new router, ie a new Mux instance
@@ -49,99 +72,78 @@ func New(rootSegment string) *Mux {
 		rootSegment = "/"
 	}
 
-	handlers := make(map[string]http.Handler)
+	isParam := make(map[string]bool)
+	handlers := make(map[string]Handler)
 	routes := make(map[string]*Route)
 
 	baseRoute := &Route{
 		Segment:  rootSegment,
-		IsParam:  false,
+		IsParam:  isParam,
 		Handlers: handlers,
 		Routes:   routes,
 	}
 
-	routes[rootSegment] = baseRoute
-
 	m := &Mux{
-		RootSegment: rootSegment,
-		Routes:      routes,
+		BaseRoute: baseRoute,
 	}
 
 	return m
 }
 
 // GET method
-func (m *Mux) GET(path string, handler http.Handler) {
+func (m *Mux) GET(path string, handler Handler) {
+	m.register(path, "GET", handler)
 }
 
 // POST method
-func (m *Mux) POST(path string, handler http.Handler) {
+func (m *Mux) POST(path string, handler Handler) {
+	m.register(path, "POST", handler)
 }
 
 // PUT method
-func (m *Mux) PUT(path string, handler http.Handler) {
+func (m *Mux) PUT(path string, handler Handler) {
+	m.register(path, "PUT", handler)
 }
 
 // DELETE method
-func (m *Mux) DELETE(path string, handler http.Handler) {
+func (m *Mux) DELETE(path string, handler Handler) {
+	m.register(path, "DELETE", handler)
 }
 
-// ListenAndServe method - we arproviding our custom router
+// ListenAndServe method - we are providing our custom router
 func (m *Mux) ListenAndServe(port string) error {
 	return http.ListenAndServe(port, m)
 }
 
 // Private methods and functions
 //----------------------------------------------------------------------------------
+func (m *Mux) register(path string, method string, handler Handler) {
+	segments, err := getSegments(path)
+	if err != nil {
+		panic(err)
+	}
+	m.doRegister(method, segments, handler)
+}
 
-func (m *Mux) register(method string, segments []string, handler http.Handler) error {
+func (m *Mux) doRegister(method string, segments []string, handler Handler) error {
 
 	// only root path registered
 	if len(segments) == 0 {
-		m.Routes[m.RootSegment].Handlers[method] = handler
+		m.BaseRoute.Handlers[method] = handler
 
 		return nil
 	}
 
-	//register the path by inserting it into the Route tree
-	var err error
+	requestPath := RequestPath{
+		Segments: segments,
+		Routes:   m.BaseRoute.Routes, // parent tree child routes
+		Method:   method,
+		Handler:  handler,
+	}
 
-	routes := m.Routes
-
-	for i, seg := range segments {
-		route, ok := routes[seg]
-
-		// a new segment => create a new child tree node/Route
-		if !ok {
-			handlers := make(map[string]http.Handler)
-			routes = make(map[string]*Route)
-			route = &Route{
-				Segment:  seg,
-				IsParam:  false,
-				Handlers: handlers,
-				Routes:   routes,
-			}
-		}
-
-		if ok, err = regexp.MatchString("^:", seg); err != nil {
-			return err
-		}
-		if ok {
-			route.IsParam = true
-		}
-
-		routes = route.Routes
-
-		sl := len(segments)
-
-		//associate the path with a handler
-		if i == (sl - 1) {
-			route.Handlers[method] = handler
-		}
-
-		routes[method] = route
-		route.Routes = routes
-
-		//segRoutes := make(map[string]*Route)
+	err := processSegment(0, requestPath)
+	if err != nil {
+		return err
 	}
 
 	return nil
